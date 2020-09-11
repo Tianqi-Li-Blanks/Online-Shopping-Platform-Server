021fe4f72fe1e2e834242491ebfe0def42c67c16
tianqi <li.tianq@husky.neu.edu>
2020-09-11 13:13:18 -0400

    build for heroku

---
+++ precache-manifest.926e12c24b05d8e57a8bc5a735232264.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaff3bde753ab1ce0a4b6c1872a73a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2d37459ad3e40ca2102",
+    "url": "/static/css/2.a17a1ed7.chunk.css"
+  },
+  {
+    "revision": "c215a92c85ed4239e837",
+    "url": "/static/css/main.66dec7cd.chunk.css"
+  },
+  {
+    "revision": "d2d37459ad3e40ca2102",
+    "url": "/static/js/2.a44fd833.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a44fd833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c215a92c85ed4239e837",
+    "url": "/static/js/main.ef0dfd67.chunk.js"
+  },
+  {
+    "revision": "21d67c3cb97b455f4033",
+    "url": "/static/js/runtime-main.d6938aff.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1b64cc74d8f9597d00889780e4cfa792",
+    "url": "/static/media/logo.1b64cc74.png"
+  }
+]);
