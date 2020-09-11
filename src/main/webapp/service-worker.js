45f3f7ca3f664352b2f22c1b17c74403b82e50b2
tianqi <li.tianq@husky.neu.edu>
2020-09-11 13:19:01 -0400

    re-build the react client and update in webapp for push in heroku

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40f66ed7b05c867aa598870f965115c9.js"
+  "/precache-manifest.926e12c24b05d8e57a8bc5a735232264.js"
 );
 
 self.addEventListener('message', (event) => {
